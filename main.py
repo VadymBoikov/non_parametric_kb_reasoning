2f1ed70318fc74e0535d4190dda6bb23ce4dc393
VadymBoikov <vadymboikov@gmail.com>
2020-05-07 13:37:58 +0300

    add argparse for dataset

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import argparse
+import multiprocessing as mp
+import os
 import numpy as np
 import pandas as pd
+
 import memory
 from graph import create_graph
 from pipeline import pipeline
@@ -7,7 +11,7 @@ from pipeline import pipeline
 cutoff = 3
 max_relations = 1000
 top_k = 5
-cores = 12
+cores = mp.cpu_count()
 
 
 def print_scores(ranks_tail, ranks_head):
@@ -17,19 +21,29 @@ def print_scores(ranks_tail, ranks_head):
     for i in [1, 3, 10]:
         hit_tail, hit_head = sum(tail < i) / size, sum(head < i) / size
         hit = np.mean([hit_tail, hit_head])
-        hit_tail, hit_head, hit = round(hit_tail, 2), round(hit_head,2), np.round(hit, 2)
-        print(f"hit @{i}: tail:{hit_tail}, head:{hit_head}, total:{hit}")
+
+        mrr_tail, mrr_head = np.mean(1 / (tail + 1)), np.mean(1 / (head + 1))
+        mrr = np.mean([mrr_tail, mrr_head])
+
+        hit, mrr = np.round(hit, 2), np.round(mrr, 2)
+        print(f"hit @{i}: {hit}, MRR: {mrr}")
 
 
 if __name__ == '__main__':
-    dataset_dir = "data/FB15k-237"  # 'data/WN18RR/original' #
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('--dataset', help='name of the dataset', default='WN18RR')
+    args = parser.parse_args()
+    dataset = args.dataset
 
-    print('load data')
-    train = pd.read_csv(f'{dataset_dir}/train.txt', sep='\t', names=['e1', 'r', 'e2'])
-    valid = pd.read_csv(f'{dataset_dir}/valid.txt', sep='\t', names=['e1', 'r', 'e2'])
+    train_dir = os.path.join("data", dataset, "train.txt")
+    valid_dir = os.path.join("data", dataset, "valid.txt")
+
+    print(f'load dataset {dataset}')
+    train = pd.read_csv(train_dir, sep='\t', names=['e1', 'r', 'e2'])
+    valid = pd.read_csv(valid_dir, sep='\t', names=['e1', 'r', 'e2'])
 
     relations_kb = memory.make_relations_kb(train, valid)
-    G = create_graph(train, add_inverse_name=True)
+    G = create_graph(train, add_inverse_name=False)
     valid = valid.loc[valid.apply(lambda x: x.e1 in G and x.e2 in G, axis=1)]  # filter nodes, that not present in train
 
     print('start cases')
@@ -37,7 +51,8 @@ if __name__ == '__main__':
     print('start similarity')
     sim_mat, node_ids = memory.create_similarity(G, sparse=True)
 
-    ranks_tail = pipeline(valid, G, sim_mat, node_ids, cases, relations_kb=relations_kb, type='tail')
-    ranks_head = pipeline(valid, G, sim_mat, node_ids, cases, relations_kb=relations_kb, type='head')
+    print('start inference')
+    ranks_tail = pipeline(valid, G, sim_mat, node_ids, cases, relations_kb=relations_kb, top_k=top_k, type='tail')
+    ranks_head = pipeline(valid, G, sim_mat, node_ids, cases, relations_kb=relations_kb, top_k=top_k, type='head')
 
     print_scores(ranks_tail, ranks_head)
